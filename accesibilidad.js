3531052556d3c1dca735e1874ce7cd54e5c3b3a9
Jose Alberto Solis <josealbertosolis@MacBook-Pro-de-Jose.local>
2020-03-03 19:41:06 +0100

    added accesibility js

---
+++ accesibilidad.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+function accessibility(elemento){
+	
+	elemento.style.backgroundColor="#303911";
+	elemento.style.color="#ebc8a7";
+	
+}
+
+function defecto(elemento){
+	
+	elemento.style.backgroundColor="#ebc8a7";
+	elemento.style.color="black";
+	
+}
