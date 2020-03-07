2466b82e2c10f30e05a6962f93c1281bc05e0da3
Jose Alberto Solis <josealbertosolis99@gmail.com>
2020-03-07 19:25:22 +0100

    uploading form files

---
+++ ClanElfos/javaScript/formulario.js
@@ -0,0 +1,28 @@
+
+window.onload = document.getElementById("enviar").disabled = true;
+
+function requerido(elemento) {
+
+    if(elemento.value == ""){
+
+        elemento.style.border = "red 2px solid";
+
+    }
+    else {
+        elemento.style.border = "green 2px solid";
+    }
+
+}
+
+function comprobar(){
+
+    if(document.getElementById("aceptar").checked && document.getElementById("lopd").checked && document.getElementById("nombre").value != "" && document.getElementById("nombreelf").value != "" && document.getElementById("cuenta").value != ""){
+
+        document.getElementById("enviar").disabled = false;
+
+    }
+    else{
+        document.getElementById("enviar").disabled = true;
+    }
+
+}
